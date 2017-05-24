cc12585992a90267eb546f60915a18e784fa4d6a
Claudia Albisetti <claudia2@banana.ch>
2017-05-24 11:51:52 +0200

    aggiornato sbaa versione 24.05.2017

--- Iva/2017/ch.banana.script.italianvatreport.2017.js
+++ Iva/2017/ch.banana.script.italianvatreport.2017.js
@@ -23,7 +23,7 @@
 // @includejs = ch.banana.script.italianvatreport.2017.xml.js
 // @includejs = ch.banana.script.italianvatreport.2017.errors.js
 // @inputdatasource = none
-// @pubdate = 2017-05-23
+// @pubdate = 2017-05-24
 // @publisher = Banana.ch SA
 // @task = export.file
 // @timeout = -1
